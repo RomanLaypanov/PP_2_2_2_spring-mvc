301a1432d7bede2a5f5a42bfd3a7951842724a8e
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-09-08 20:34:14 +0300

    Первая проверка

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,44 +3,31 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.services.CarServices;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/cars")
 public class CarController {
 
-    @GetMapping
+    @GetMapping(value = "/cars")
     public String carShow(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        List<Car> allCars = carCreat();
+        CarServices carServices = new CarServices();
         List<Car> carsToShow;
 
         if (count == null) {
-            carsToShow = allCars;
-        } else if (count <= carCreat().size() && count > 0) {
-            carsToShow = allCars.subList(0, count);
+            carsToShow = carServices.carCreat();
+        } else if (count <= carServices.carCreat().size() && count > 0) {
+            carsToShow = carServices.carCreat().subList(0, count);
         } else {
-            carsToShow = allCars;
+            carsToShow = carServices.carCreat();
         }
 
         model.addAttribute("cars", carsToShow);
+        model.addAttribute("number", carServices.quantityCarsFromList());
 
         return "cars";
-    }
-
-    private List<Car> carCreat() {
-        List<Car> carList = new ArrayList<>();
-
-        carList.add(new Car("Mercedes-Benz", "Black", 7_900_000));
-        carList.add(new Car("BMW", "White", 5_600_000));
-        carList.add(new Car("Volkswagen", "Silver", 1_200_000));
-        carList.add(new Car("Ford", "Yellow", 790_000));
-        carList.add(new Car("Kia", "Grey", 2_300_000));
-
-        return carList;
     }
 }
